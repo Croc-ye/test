4880ae726f531ee87f5141684fb282ded1bd471f
vimi <vimiming@gmail.com>
2018-12-18 22:12:53 +0800

    Add go file

--- makefile
+++ makefile
@@ -3,5 +3,8 @@
 build-cpp:
 	@./shell/total-run.sh build-cpp
 
+build-go:
+	@./shell/total-run.sh build-go
+
 clean:
 	rm -rf ./cpp/main
--- shell/total-run.sh
+++ shell/total-run.sh
@@ -4,11 +4,20 @@ function build-cpp() {
   g++ ./cpp/main.cpp -o ./cpp/main && ./cpp/main
 }
 
+function build-go() {
+  go run ./go/image.go
+}
+
 
 case "$1" in
   build-cpp)
     build-cpp
     ;;
+
+  build-go)
+    build-go
+    ;;
+    
   *)
     echo "please choose one {dump | restore}"
     exit 1
