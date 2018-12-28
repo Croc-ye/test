0a9212e3cad1e1157fbb41898c21e9a8fe6aa17b
vimi <vimiming@gmail.com>
2018-12-28 22:50:17 +0800

    basic react file

---
+++ python/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: vimiliu
+# @Date:   2018-12-28 22:36:25
+# @email: vimiliu@gmail.com
+
+# @Last Modified by:   vimiliu
+# @Last Modified time: 2018-12-28 22:36:27
