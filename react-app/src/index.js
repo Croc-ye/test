cf583e67a3873474924cfddea3f72c1b180dc3d5
vimiPonyai <weimingliu@pony.ai>
2019-01-07 20:59:34 +0800

    react-app hello world button

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,1 +1,13 @@
-'use strict'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
