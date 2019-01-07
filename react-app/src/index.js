5c28a0305f316d355b16fb357f3fbe496ecb72a9
vimiPonyai <weimingliu@pony.ai>
2019-01-07 21:13:52 +0800

    material-ui commpent

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-function App() {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  );
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <DialogContentText>1-2-3-4-5</DialogContentText>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Index />,
+  document.getElementById('root'),
+);
