781085ec29eee2f182db84f910379c29d6769c76
vimiPonyai <weimingliu@pony.ai>
2019-01-08 09:38:53 +0800

    Revert "signIn"
    This reverts commit aa53c028021ca10388acba547095505fd7447151.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,10 +1,36 @@
-import withStyles from './singin.js';
-import ReactDOM from 'react-dom';
 import React from 'react';
-console.log(withStyles);
-const SignIn = withStyles.SignIn;
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <DialogContentText>1-2-3-4-5</DialogContentText>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <SignIn />,
+  <Index />,
   document.getElementById('root'),
 );
