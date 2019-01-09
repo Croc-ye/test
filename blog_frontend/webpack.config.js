d9ed495b4c5eda2ed3949fb4164b57ddfc4c7852
vimiPonyai <weimingliu@pony.ai>
2019-01-09 16:23:04 +0800

    Docker for npm build

--- blog_frontend/webpack.config.js
+++ blog_frontend/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   entry: path.join(__dirname, "./app/index.js"),
   output: {
     path: path.join(__dirname, "build"),
+    publicPath:'/',
     filename: "index.js"
   },
   devServer: {
