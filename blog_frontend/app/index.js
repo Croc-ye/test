0fc215bd94e6cea4a756946f3f6802f2ed1843fd
vimiPonyai <weimingliu@pony.ai>
2019-01-09 20:53:00 +0800

    react-router

--- blog_frontend/app/index.js
+++ blog_frontend/app/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
 import Login from './components/login.js';
 
+class  AppRouter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <ul>
+            <Link to="/login"> goto </Link>
+          </ul>
+          <Route exact path="/*" component={Login}/>
+          <Route path="/login" component={Login}/>
+        </div>
+      </BrowserRouter>
+    );
+  }
+};
+
 ReactDOM.render(
-  <Login />,
+  <AppRouter />,
   document.getElementById('root'),
 );
