1a62f46b1288b656547d80a1af8e8d17334d3269
vimi <vimiming@gmail.com>
2019-01-20 13:22:03 +0800

    [Frontend] add write_blog_icon

--- blog_frontend/app/components/person.js
+++ blog_frontend/app/components/person.js
@@ -5,7 +5,9 @@ import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import withStyles from '@material-ui/core/styles/withStyles';
-
+import Fab from '@material-ui/core/Fab';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 import LockIcon from '@material-ui/icons/LockOutlined';
 import BlogList from '../components/bloglist.js';
 import mrouter from '../common/mrouter.js';
@@ -26,11 +28,24 @@ const styles = theme => ({
     margin: theme.spacing.unit,
   },
   root: {
-    display: 'flex',
+    'display': 'flex',
+    'flex-directon': 'row',
+    'justify-content': 'space-between',
   },
   username: {
-    marginTop: theme.spacing.unit * 4,
-    marginLeft: theme.spacing.unit,
+    'display': 'flex',
+    'margin-top': theme.spacing.unit * 4,
+    'justify-content': 'left',
+    'margin-left': theme.spacing.unit,
+  },
+  write_blog: {
+  },
+  write_blog_icon: {
+    'margin-top': theme.spacing.unit * 2,
+  },
+  avatar_with_username: {
+    'display': 'flex',
+    'flex-directon': 'row',
   }
 });
 
@@ -99,9 +114,21 @@ class Person extends React.Component {
     return (
       <main className={classes.main}>
         <div className={classes.root}>
-          <Avatar src={this.state.user.avatar} className={classes.avatar_big} > User </Avatar>
-          <div className={classes.username}> {this.state.user.username} </div>
+          <div className={classes.avatar_with_username}>
+            <Avatar src={this.state.user.avatar} className={classes.avatar_big} > User </Avatar>
+            <div className={classes.username}>
+              {this.state.user.username} 
+            </div>
+          </div>
+
+          <div className={classes.write_blog}>
+            <Fab size="small" color="secondary" aria-label="Add" className={classes.write_blog_icon}>
+              <AddIcon />
+            </Fab>
+          </div>
+
         </div>
+
         <hr />
         <hr />
         {this.converBlogs(this.state.blogs)}
