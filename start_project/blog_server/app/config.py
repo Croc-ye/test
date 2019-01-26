b95445ea617685fce69961dd8c4551a5d3407852
vimi <vimiming@gmail.com>
2019-01-26 21:53:01 +0800

    [MOVE] move to start project

--- start_project/blog_server/app/config.py
+++ start_project/blog_server/app/config.py
@@ -8,11 +8,11 @@ class db_config():
     USER = os.getenv('MYSQL_ROOT_USER', default = 'root')
     PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD', default = 'root')
     DATABASE = os.getenv('MYSQL_DATABASE', default = 'dev')
-    HOST = os.getenv('DB_HOST', default = '172.18.0.3')
+    HOST = os.getenv('DB_HOST', default = '172.21.0.3')
 
 class redis_config():
     PROT = os.getenv('REDIS_PROT', default = '6379')
-    HOST = os.getenv('REDIS_HOST', default = '172.18.0.2')
+    HOST = os.getenv('REDIS_HOST', default = '172.21.0.2')
 
 class blog_server_config():
     SECRET_KEY = os.getenv('SECRET_KEY', default = 'no_secret')
