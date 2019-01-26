ddb360fa75eceb28c6da38d7bd591b72824b6d1d
vimiPonyai <weimingliu@pony.ai>
2019-01-26 21:37:10 +0800

    [Change] update file

--- blog_server/app/cache/util.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-def format_user_blog_id_key(user_id: int) -> 'str':
-    return "REDIS_USER_BLOG_ID_KEY:{}".format(str(user_id))
-
-def format_user_avatar_key(user_id: int) -> 'str':
-    return "REDIS_USER_AVATAR_KEY:{}".format(str(user_id))
