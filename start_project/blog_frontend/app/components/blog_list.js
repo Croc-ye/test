c495d60803f95111fbd79cc02bfebf3cbcea6be4
vimi <vimiming@gmail.com>
2019-01-27 23:16:24 +0800

    [Frontend] add love this page

--- start_project/blog_frontend/app/components/blog_list.js
+++ start_project/blog_frontend/app/components/blog_list.js
@@ -7,6 +7,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
+import Badge from '@material-ui/core/Badge';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
@@ -68,10 +69,21 @@ class BlogList extends React.Component {
     }
   }
 
-  onLoveOrNotClick() {
-    this.setState({
-      loveId: this.state.loveId === 0 ? 1 : 0,
-    });
+  onLoveOrNotClick(idx, username, user_blog_id) {
+    // this.setState({
+    //   loveId: this.state.loveId === 0 ? 1 : 0,
+    // });
+    api.request(`${config.blogLove}${username}/${user_blog_id}/`).then(
+      (success) => {
+        this.state.blogs[idx].love += success.success;
+        this.setState({
+          blogs: this.state.blogs,
+        });
+      },
+      (error) => {
+        log.error(error);
+      }
+    );
   }
 
   componentWillMount() {
@@ -114,11 +126,16 @@ class BlogList extends React.Component {
 
         <CardActions className={classes.actions} disableActionSpacing>
 
-          <IconButton aria-label="Add to favorites" style={{"color":this.state.loveOrNot[this.state.loveId]}} onClick={(e)=>{this.onLoveOrNotClick()}}>
-            <FavoriteIcon />
+          <IconButton 
+            aria-label="Add to favorites" 
+            style={{"color":this.state.loveOrNot[this.state.loveId]}} 
+            onClick={(e)=>{this.onLoveOrNotClick(idx, blog.user.username, blog.user_blog_id)}}>
+              <Badge badgeContent={blog.love} color="secondary">
+                <FavoriteIcon />
+              </Badge>
           </IconButton>
 
-          <IconButton aria-label="Share">
+          <IconButton aria-label="Share" onClick={(e)=>{mrouter.goToBlogPage(blog.user.username, blog.user_blog_id)}}>
             <ShareIcon />
           </IconButton>
 
