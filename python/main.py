4f64ab399fb29b63276435f8b71a6655a3c2fccb
vimiPonyai <weimingliu@pony.ai>
2019-01-29 21:40:08 +0800

    [Python] socket

--- python/main.py
+++ python/main.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-# @Author: vimiliu
-# @Date:   2018-12-28 22:36:25
-# @email: vimiliu@gmail.com
+import socket
+sk=socket.socket()
+address=('127.0.0.1', 8888)
 
-# @Last Modified by:   vimiliu
-# @Last Modified time: 2018-12-28 22:36:27
+sk.bind(address)
+sk.listen()
+
+while True:
+    print('等待客户端链接' + '.' * 20)
+    connect, client = sk.accept()
+    print(str(client)+'上线')
+    while True:
+        try:
+            data = connect.recv(2048)
+            if not data:break
+            print(str(data, 'utf8'))
+            connect.send(bytes("aaaa", 'utf8'))
+        except ConnectionResetError:
+            print('-'*10+'对方离线'+'-'*10)
+            break
+
+sk.close()
